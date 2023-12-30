6111c9bd7d365589a9082c5678591ebc3d7e4df2
ProfessorGundar <RyanDL1@hotmail.com>
2023-12-30 15:18:09 -0700

    Got room add button working

--- cyberpunk_tycoon/src/Components/Building.tsx
+++ cyberpunk_tycoon/src/Components/Building.tsx
@@ -5,104 +5,73 @@ import { Room } from "./Room";
 import * as PIXI from "pixi.js";
 
 import { useAtom } from "jotai";
+import { useState } from "react";
 import { roomsListAtom, roomSelectors } from "../GameState/Room";
 import { Room as roomInterface } from '../interface/Room';
 
 export function Building(){
 
-    const bW = 500;
-    const bH = 600;
-    
-    const bX = 0;
-    const bY = 0;
-    
-    const rWPadding = 0.025;
-    const rHPercent = 0.4;
 
-    const rW = bW - (bW * rWPadding * 2);
+    const nextRoomXCoordinate = 0;
+    const nextRoomYCoordinate = 0;
 
-    const rH = bH * rHPercent;
-    const rX = bX + (bW * rWPadding);
-    const rY = (bY + bH) - rH;
+
+    const buildingWidth = 500;
+    const buildingHeight = 600;
+    const buildingXCoordinate = 0;
+    const buildingYCoordinate = 0;
+    
+    const roomWidthPaddingPercent = 0.025;
+    const roomHeightPercent = 0.4;
+    const roomWidth = buildingWidth - (buildingWidth * roomWidthPaddingPercent * 2);
+    const roomHeight = buildingHeight * roomHeightPercent;
+    const roomXCoordinate = buildingXCoordinate + (buildingWidth * roomWidthPaddingPercent);
+    const roomYCoordinate = (buildingYCoordinate + buildingHeight) - roomHeight;
+    const [roomGraphicShapeList, setGraphicShapeList] = useState<any>([]);
     
     const drawBuilding = useCallback((g: pixiGraphics) => {
         g.clear();
         g.beginFill(0xffffff);
-        g.drawRect(bX, bY, bW, bH);
+        g.drawRect(buildingXCoordinate, buildingYCoordinate, buildingWidth, buildingHeight);
         g.endFill();
     },[])
 
-    const makeButton = useCallback((g: pixiGraphics) => {
-        g.clear();
-        g.beginFill(0x8c3b0c);
-        g.drawRect(0, 200, 100, 100);
-        g.endFill();
-    },[])
-/*
-    function renderRoom(this: typeof Room){
-        <Room rW={rW} rH={rH} rX={rX} rY={rY} />
-    }
-    */
-/*                <button onClick={() => addRoom({
-                    upgradeLevel: 2,
-                    cost: 150,
-                    baseIncome: 15,
-                    customerPresent: false,
-                    numOfEmployees: 1,
-                    baseMaintanceModifier: 120,
-                    baseTimeTaskCompletion: 3,
-                    taskComplete: false,
-                    })}>Add Room
-                </button>
-*/
     const updateBuilding = useCallback((g: pixiGraphics) => {
         g.clear();
     },[])
 
     const makeRoomInBuilding = (e:any) => {
-        console.log("Hello");
+        console.log(e);
+        console.log("Making a new room");
+        const roomGraphicShape = {x: roomXCoordinate, y: roomYCoordinate, w: roomWidth, h: roomHeight};
+        const newData = [...roomGraphicShapeList, roomGraphicShape];
+        setGraphicShapeList(newData);
     };
+
+    const gButton = useCallback((g:pixiGraphics) => {
+        g.clear();
+        g.beginFill(0x8c3b0c);
+        g.drawRect(0, 200, 100, 100);
+        g.endFill();
+    },[])
+
     const [roomsList, setRoomsList] = useAtom(roomsListAtom);
-
-    const addRoom = (newRoom: roomInterface) => {
-        setRoomsList((prevList: any) => [...prevList, newRoom]);
-    };
-
-    //const text = roomsList[2].cost;
 
     return(
         <Container>
             <Container x={500} y={100}>
-                <Graphics
-                    draw={drawBuilding}
-                    eventMode="static" //Makes it interactable
-                    cursor="pointer" // Adds a hand on windows machine only
-                    //onclick={test}
-                    interactive={true}
-                    //pointerdown={() => {console.log("HI")}}
-                    //pointermove={() => {console.log("HI")}}
-                    //mousedown={() => {console.log("HI")}}
-                    //mousemove={() => {console.log("HI")}}
-                    //mouseover={() => {console.log("HI")}}
-                    //mouseout={() => {console.log("HI")}}
-                    //onclick={() => {console.log("HI")}}
-                    //hitArea={new PIXI.Rectangle(0,0, 100, 100)}
-                />
-                <Room rW={rW} rX={rX}rH={rH}rY={rY}/>
+                <Graphics draw={drawBuilding}/>
+                {roomGraphicShapeList.map((roomGraphicShape:any, index: any)=> {
+                    return (
+                        <Room rW={roomGraphicShape.w} rX={roomGraphicShape.x} rH={roomGraphicShape.h} rY={roomGraphicShape.y}/>
+                    );
+                })}
             </Container>
             <Graphics
-                    draw={makeButton}
+                    draw={gButton}
                     eventMode="static" //Makes it interactable
                     cursor="pointer" // Adds a hand on windows machine only
                     onclick={makeRoomInBuilding}
-                    //interactive={true}
-                    //pointerdown={() => {console.log("HI")}}
-                    //pointermove={() => {console.log("HI")}}
-                    //mousedown={() => {console.log("HI")}}
-                    //mousemove={() => {console.log("HI")}}
-                    //mouseover={() => {console.log("HI")}}
-                    //mouseout={() => {console.log("HI")}}
-                    //onclick={() => {console.log("HI")}}
                     hitArea={new PIXI.Rectangle(0, 200, 100, 100)}
                 />
         </Container>
